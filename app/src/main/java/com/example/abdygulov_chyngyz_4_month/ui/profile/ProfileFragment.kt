1445e6cc7a3121add4ef683538656c5524dfe9da
Abdygulovv <chyngyz.abdygulov.2002.08.10@gmail.com>
2023-06-11 15:17:09 +0600

    Home work 1

---
+++ app/src/main/java/com/example/abdygulov_chyngyz_4_month/ui/profile/ProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.example.abdygulov_chyngyz_4_month.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.abdygulov_chyngyz_4_month.R
+import com.example.abdygulov_chyngyz_4_month.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
